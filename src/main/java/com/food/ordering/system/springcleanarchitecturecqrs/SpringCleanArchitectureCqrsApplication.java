56b7c5a7817c703c028a987d5738d23df7f272cc
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-09-03 16:50:53 +0300

    Initial commit

---
+++ src/main/java/com/food/ordering/system/springcleanarchitecturecqrs/SpringCleanArchitectureCqrsApplication.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.springcleanarchitecturecqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCleanArchitectureCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCleanArchitectureCqrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/food/ordering/system/springcleanarchitecturecqrs/SpringCleanArchitectureCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.ordering.system.springcleanarchitecturecqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCleanArchitectureCqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
