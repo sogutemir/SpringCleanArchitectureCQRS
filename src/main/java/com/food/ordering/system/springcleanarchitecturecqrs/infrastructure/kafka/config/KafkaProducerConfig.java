ca92ec05848f4b8e7a3afd8391408121f0e8d28e
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-09-07 14:52:44 +0300

    Update KafkaProducerConfig.java

--- src/main/java/com/food/ordering/system/springcleanarchitecturecqrs/infrastructure/kafka/config/KafkaProducerConfig.java
+++ src/main/java/com/food/ordering/system/springcleanarchitecturecqrs/infrastructure/kafka/config/KafkaProducerConfig.java
@@ -26,7 +26,22 @@ public class KafkaProducerConfig {
     }
 
     @Bean
+    public ProducerFactory<String, String> stringProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
     public KafkaTemplate<String, Object> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaStringTemplate() {
+        return new KafkaTemplate<>(stringProducerFactory());
+    }
+
 }
